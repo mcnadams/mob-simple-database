6d1244471be618ae9fab4717e02749cda4cdd4d8
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-04 10:03:29 -0700

    gived saved object an id

--- test/Store.test.js
+++ test/Store.test.js
@@ -1,7 +1,15 @@
 const Store = require('../lib/Store');
 const path = require('path');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
 
 describe('Store class', () => {
+  beforeEach(done => {
+    mkdirp('./animals', done);
+  });
+  afterEach(done => {
+    rimraf('./animals', done);
+  });
   it('binds directory path in store', () => {
     const rootDirectory = path.join(__dirname, 'animals');
     const store = new Store(rootDirectory);
@@ -12,11 +20,11 @@ describe('Store class', () => {
     const rootDirectory = path.join(__dirname, 'animals');
     const store = new Store(rootDirectory);
     const kittems = {
-      name: 'Piffen'
+      name: 'Kizmar'
     }
     store.create(kittems, (err, savedObject) => {
       expect(savedObject._id).toEqual(expect.any(String));
-
+      done();
     });
     
   })
